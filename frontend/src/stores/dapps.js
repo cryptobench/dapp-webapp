a856af7a355098c6a9c00eaaa5e905b452e72ffe
Marcin Gordel <marcin.gordel@gmail.com>
2022-10-05 12:53:30 +0200

    Fixed refreshing data on the detail page (#25)

--- frontend/src/stores/dapps.js
+++ frontend/src/stores/dapps.js
@@ -7,18 +7,21 @@ const userStore = useUserStore();
 export const useDappsStore = defineStore("dapps", {
   state: () => ({
     dapps: [],
-    stateData: "",
-    rawData: "",
-    stdout: "",
-    stderr: "",
-    log: "",
-    running: false,
+    stateData: {},
+    rawData: {},
+    stdout: {},
+    stderr: {},
+    log: {},
+    running: {},
   }),
 
   getters: {
-    getDapp: (state) => {
-      return (id) => state.dapps.find((dapp) => dapp.id === id);
-    },
+    getDapp: (state) => (id) => state.dapps.find((dapp) => dapp.id === id),
+    getStateData: (state) => (id) => state.stateData[id],
+    getRawData: (state) => (id) => state.rawData[id],
+    getStdout: (state) => (id) => state.stdout[id],
+    getStderr: (state) => (id) => state.stderr[id],
+    getLog: (state) => (id) => state.log[id],
   },
 
   actions: {
@@ -38,24 +41,31 @@ export const useDappsStore = defineStore("dapps", {
       return !!id;
     },
     async getData(id) {
-      this.stateData = await api.get(`/dapp/rawState/${id}`);
-      this.rawData = await api.get(`/dapp/rawData/${id}`);
-      this.stdout = await api.get(`/dapp/stdout/${id}`);
-      this.stderr = await api.get(`/dapp/stderr/${id}`);
-      this.log = await api.get(`/dapp/log/${id}`);
+      this.stateData[id] = await api.get(`/dapp/rawState/${id}`);
+      this.rawData[id] = await api.get(`/dapp/rawData/${id}`);
+      this.stdout[id] = await api.get(`/dapp/stdout/${id}`);
+      this.stderr[id] = await api.get(`/dapp/stderr/${id}`);
+      this.log[id] = await api.get(`/dapp/log/${id}`);
     },
     async startGettingData(id) {
-      this.running = true;
-      this.statusData = "";
-      this.rawData = "";
+      this.running[id] = true;
+      this.clearData(id);
       const start = async () => {
         await this.getData(id);
-        if (this.running) setTimeout(async () => await start(), 5000);
+        if (this.running[id]) setTimeout(async () => await start(), 5000);
       };
       start();
     },
-    stopGettingData() {
-      this.running = false;
+    stopGettingData(id) {
+      this.clearData(id);
+      this.running[id] = false;
+    },
+    clearData(id) {
+      this.statusData?.[id] && (this.statusData[id] = "");
+      this.rawData?.[id] && (this.rawData[id] = "");
+      this.stdout?.[id] && (this.stdout[id] = "");
+      this.stderr?.[id] && (this.stderr[id] = "");
+      this.log?.[id] && (this.log[id] = "");
     },
   },
 });
