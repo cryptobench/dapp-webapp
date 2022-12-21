3a25943104a0354fb765b4ef1b971b679b935059
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2022-12-21 15:39:53 +0100

    Making the pipeline more strict

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
 
   parserOptions: {
     parser: "@babel/eslint-parser",
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
   },
 
@@ -57,7 +57,7 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    //"prefer-promise-reject-errors": "off",
+    "prefer-promise-reject-errors": "off",
 
     // allow debugger during development only
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
