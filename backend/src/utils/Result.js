a92dc79d9c613fce16225825fd99bf1b0076f8ae
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-01-12 15:39:23 +0100

    Fix adding error code to UserError not being used
    When adding a response code to the UserError function it returned 400 regardless of what you configured.
    
    400 was hardcoded inside the routes file. I have modified it to use the code provided or default to 400 now.

--- backend/src/utils/Result.js
+++ backend/src/utils/Result.js
@@ -7,7 +7,7 @@ class UserError extends Error {
   constructor(message, code) {
     super(message);
     this.name = "UserError";
-    this.code = code;
+    this.code = code ? code : 400;
   }
   toJson() {
     return {
